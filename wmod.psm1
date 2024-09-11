5e79b833d8302b7be94fec38137f422356a7093b
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-09-11 19:34:50 +0800

    feat: wmod-list

--- wmod.psm1
+++ wmod.psm1
@@ -1,6 +1,8 @@
 $Wmod_Path = "C:\Program Files\PowerShell\7\Modules\Wmod"
 $Wmod_ModulePath = "$Wmod_Path\ModuleFiles"
 
+$script:Wmod_Loaded = @()
+
 function Wmod-Avail {
     Get-ChildItem $Wmod_ModulePath -Directory | ForEach-Object {
         $name = $_.Name
@@ -22,6 +24,7 @@ function Wmod-Load {
     if (Test-Path $modulePath) {
         . $modulePath
         $env:PATH = $Path + $env:PATH
+        $script:Wmod_Loaded += $modulePath
         Write-Host "$modulePath module loaded."
     }
     else {
@@ -40,9 +43,17 @@ function Wmod-Unload {
     if (Test-Path $modulePath) {
         . $modulePath
         $env:PATH = $env:PATH.Replace($Path, '')
+        $script:Wmod_Loaded = $script:Wmod_Loaded | Where-Object { $_ -ne $modulePath }
         Write-Host "$modulePath module unloaded."
     }
     else {
         Write-Host "$modulePath module not found."
+    }
+}
+
+function Wmod-List {
+    Write-Host "Loaded modules:"
+    $script:Wmod_Loaded | ForEach-Object {
+        Write-Host $_
     }
 }
